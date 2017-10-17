a3b8ae7268ada2f36fba95183160f1334d3dca9e
luowh <luowh@chinasap.cn>
2017-10-17 17:35:42 +0800

    Signed-off-by: luowh <luowh@chinasap.cn>

---
+++ app/src/main/java/com/example/fangl/webviewtest/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.fangl.webviewtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = (WebView)findViewById(R.id.web_view);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://rmis.ideasoft.net.cn");
+    }
+}
